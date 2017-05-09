9cb317d06c5381c7f912cb41955901fa55139dac
吴 <wu@wudeMacBook-Pro.local>
2017-05-09 12:45:03 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  has_many :groups
-  has_many :posts
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :groups
+  has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
 
@@ -14,4 +14,12 @@ class User < ApplicationRecord
       participated_groups.include?(group)
     end
 
+    def join!(group)
+        participated_groups << group
+      end
+
+      def quit!(group)
+        participated_groups.delete(group)
+      end
+      
 end
